9bae606feda3eceff1bac9b83868be041d51c1ed
BelkanWar <challenger1501@gmail.com>
2022-03-22 18:06:10 +0800

    new progress

--- src/main_process.py
+++ src/main_process.py
@@ -3,10 +3,11 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 import utils
 import torch
 import numpy as np
+from sklearn import metrics
 
 # parameters
-EPOCH = 50
-LEARNING_RATE = 0.05
+EPOCH = 300
+LEARNING_RATE = 0.03
 target = 'Pipe insulation'
 explain = ['EPC category',
            'EPC type',
@@ -20,27 +21,53 @@ explain = ['EPC category',
            'Exhaust','Balanced',
            'Balanced with heat exchanger',
            'Exhaust with heat pump',
-           'Natural ventilation']
+           'Natural ventilation',
+           'Gbg']
 usecols = [target] + explain
+
+AUC = []
+F1 = []
 
 
 data = utils.read_data('../data/Dwelling_asbestos_fixed.csv', usecols)
-tensor_Dataset = utils.prepare_tensor(data, target)
-train_loader, test_loader = utils.dataset_split(tensor_Dataset, len(data))
+
+for train_data, test_data in utils.data_prepare_and_split(data, 3):
+    train_loader = utils.prepare_tensor(train_data, target)
+    test_loader = utils.prepare_tensor(test_data, target)
 
 
-# modeling
-model = utils.Model(len(set(data['EPC category'])), len(set(data['EPC type'])))
-loss_func = torch.nn.BCELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr = LEARNING_RATE)
+    # modeling
+    model = utils.Model(len(set(data['EPC category'])), len(set(data['EPC type'])))
+    loss_func = torch.nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr = LEARNING_RATE)
+    # scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma = 0.96, verbose=True)
 
-for epoch in range(1, EPOCH+1):
-    for batch_data in train_loader:
-        prediction = model(batch_data)
-        loss = loss_func(prediction, batch_data[0].unsqueeze(1))
+    for epoch in range(1, EPOCH+1):
+        prediction = model(train_loader.tensors)
+        loss = loss_func(prediction, train_loader.tensors[0].unsqueeze(1))
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        
+        # scheduler.step()
+        # print(f"training loss: {float(loss.data)}")
+    
+    model.eval()
 
-        print(f"training loss: {float(loss.data)}")
+    predict_proba = model(test_loader.tensors).detach().numpy().reshape((-1))
+    predict = np.array([0 if i <0.5 else 1 for i in predict_proba])
+    y_true = test_loader.tensors[0].numpy().astype(int)
+    fpr, tpr, thresholds = metrics.roc_curve(y_true, predict_proba, pos_label=1)
+    AUC.append(metrics.auc(fpr, tpr))
+    F1.append(metrics.f1_score(y_true, predict))
+    print('predict: ',predict)
+    print('true:    ', y_true)
+    print('')
+
+
+print(AUC)
+print(np.average(AUC))
+print(F1)
+print(np.average(F1))
+
 
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import torch
 import torch.nn.functional as F
+import numpy as np
+from sklearn.model_selection import KFold
 
 def integer_encoding_dict(input):
     return {string:idx for idx, string in enumerate(list(set(input)))}
@@ -11,14 +13,36 @@ def read_data(path, usecols):
     data = data.dropna()
     return data
 
-def prepare_tensor(data, target):
+def data_prepare_and_split(data, n_fold):
     EPC_cate_to_idx_dict = integer_encoding_dict(data['EPC category'])
     EPC_type_to_idx_dict = integer_encoding_dict(data['EPC type'])
 
+    data['EPC category'] = [EPC_cate_to_idx_dict[i] for i in data['EPC category']]
+    data['EPC type'] = [EPC_type_to_idx_dict[i] for i in data['EPC type']]
+
+    for key in list(data):
+        data[key] = np.array(data[key])
+    
+    kf = KFold(n_splits=n_fold, shuffle=True)
+    output = []
+
+    for train_idx, test_idx in kf.split(data[key]):
+        train_data, test_data = {}, {}
+        for key in data:
+            train_data[key] = np.array(data[key])[train_idx]
+            test_data[key] = np.array(data[key])[test_idx]
+
+        output.append([train_data, test_data])
+
+    return output
+
+
+def prepare_tensor(data, target):
+
     tensor_dataset = torch.utils.data.TensorDataset(
         torch.FloatTensor(list(data[target])),
-        torch.LongTensor([EPC_cate_to_idx_dict[i] for i in data['EPC category']]),
-        torch.LongTensor([EPC_type_to_idx_dict[i] for i in data['EPC type']]),
+        torch.LongTensor(list(data['EPC category'])),
+        torch.LongTensor(list(data['EPC type'])),
         torch.FloatTensor(list(data['Construction year'])),
         torch.FloatTensor(list(data['Renovation year'])),
         torch.FloatTensor(list(data['Number of floors'])),
@@ -29,32 +53,24 @@ def prepare_tensor(data, target):
         torch.FloatTensor(list(data['Balanced'])),
         torch.FloatTensor(list(data['Balanced with heat exchanger'])),
         torch.FloatTensor(list(data['Exhaust with heat pump'])),
-        torch.FloatTensor(list(data['Natural ventilation']))
+        torch.FloatTensor(list(data['Natural ventilation'])),
+        torch.FloatTensor(list(data['Gbg']))
     )
 
     return tensor_dataset
-
-def dataset_split(tensor_dataset, dp_count):
-
-    train_dataset, test_dataset = torch.utils.data.random_split(tensor_dataset, 
-        [dp_count-int(dp_count/8), int(dp_count/8)])
-    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size = 1000, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size = 1000)
-
-    return train_loader, test_loader
 
 
 class Model(torch.nn.Module):
     def __init__(self, epc_cate_count, epc_type_count):
         super(Model, self).__init__()
-        epc_cate_hidden_dim = 6
-        epc_type_hidden_dim = 6
+        epc_cate_hidden_dim = 4
+        epc_type_hidden_dim = 4
         self.epc_cate_embed = torch.nn.Embedding(epc_cate_count, epc_cate_hidden_dim)
         self.epc_type_embed = torch.nn.Embedding(epc_type_count, epc_type_hidden_dim)
-        self.bn0 = torch.nn.BatchNorm1d(11+epc_cate_hidden_dim+epc_type_hidden_dim)
+        self.bn0 = torch.nn.BatchNorm1d(12+epc_cate_hidden_dim+epc_type_hidden_dim)
         self.bn1 = torch.nn.BatchNorm1d(10)
         self.bn2 = torch.nn.BatchNorm1d(5)
-        self.linear1 = torch.nn.Linear(11+epc_cate_hidden_dim+epc_type_hidden_dim,10)
+        self.linear1 = torch.nn.Linear(12+epc_cate_hidden_dim+epc_type_hidden_dim,10)
         self.linear2 = torch.nn.Linear(10,5)
         self.linear3 = torch.nn.Linear(5,1)
         self.sigmoid = torch.nn.Sigmoid()
@@ -66,7 +82,7 @@ class Model(torch.nn.Module):
             x_input[4].unsqueeze(1), x_input[5].unsqueeze(1), x_input[6].unsqueeze(1),
             x_input[7].unsqueeze(1), x_input[8].unsqueeze(1), x_input[9].unsqueeze(1),
             x_input[10].unsqueeze(1), x_input[11].unsqueeze(1), x_input[12].unsqueeze(1),
-            x_input[13].unsqueeze(1)), dim=1)
+            x_input[13].unsqueeze(1), x_input[14].unsqueeze(1)), dim=1)
         linear_vector = F.relu(self.bn0(linear_vector))
         linear_vector = F.relu(self.bn1(self.linear1(linear_vector)))
         linear_vector = F.relu(self.bn2(self.linear2(linear_vector)))
